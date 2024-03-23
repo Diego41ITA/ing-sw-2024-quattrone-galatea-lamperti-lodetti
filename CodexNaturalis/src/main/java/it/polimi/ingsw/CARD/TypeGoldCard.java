bc7ef780c515c0850d2a115ae5cbbeb2015e781c
AlessandroLodetti <alessandro.lodetti@mail.polimi.it>
2024-03-23 17:49:42 +0100

    commit of the two enum TypeOfCard and TypeOfGoldCard.

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/CARD/TypeGoldCard.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.CARD;
+
+public enum TypeOfGoldCard {
+    ANGLE,  //gives points for the covered angle
+    CLASSIC,    //gives points with no restriction
+    POISON,
+    PARCHMENT,
+    FEATHER;
+}
---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/CARD/TypeOfCard.java
@@ -0,0 +1,9 @@
+package it.polimi.ingsw.CARD;
+
+public enum TypeOfCard {
+    ANIMAL,
+    VEGETABLE,
+    MUSHROOM,
+    INSECT,
+    STARTING;   //this one might be useless
+}
