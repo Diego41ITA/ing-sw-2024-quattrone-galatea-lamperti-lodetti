54826e12c21bf78f3189ffb69620bad1bd1da2cb
AlessandroLodetti <alessandro.lodetti@mail.polimi.it>
2024-03-24 08:45:15 +0100

    commit the Deck class, getFirst needs to throw an exception.

--- CodexNaturalis/src/main/java/it/polimi/ingsw/card/Deck.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/card/Deck.java
@@ -1,7 +1,31 @@
 package it.polimi.ingsw.card;
 
-import java.util.List;
+import java.util.*;
 
 public class Deck {
     private List<Card> cards;
+
+    public Deck(List<Card> deck){
+        this.cards = new ArrayList<>(deck);
+    }
+
+    public Card getFirst(){
+        int index = this.getDimension() - 1;
+        if(index > 0)
+            return cards.remove(index); //return the last card
+        else
+            //---------
+            //throws an exception
+            return null;
+            //ATTENTION
+            //---------
+    }
+
+    public void shuffle(){
+        Collections.shuffle(cards);
+    }
+
+    public int getDimension(){
+        return cards.size();
+    }
 }
