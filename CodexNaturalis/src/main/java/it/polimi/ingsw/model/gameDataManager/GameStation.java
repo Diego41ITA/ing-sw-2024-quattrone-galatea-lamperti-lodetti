aed513bd38b36d150ed92c28b13c448cbee1ecb0
LorenzoGalatea <161478338+LorenzoGalatea@users.noreply.github.com>
2024-03-26 15:45:22 +0100

    Update GameStation.java

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/gameDataManager/GameStation.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/gameDataManager/GameStation.java
@@ -1,4 +1,45 @@
 package it.polimi.ingsw.model.gameDataManager;
+import java.util.*;
 
-public class GameStation {
+/** @author 
+ * this class represents the Station where the player places his cards for make point
+ */
+class GameStation {
+    private Map<Card, List<Coord>> playedCards;
+
+    public GameStation() {
+        playedCards = new Map<>;
+    }
+
+    public placeCard(){
+    }
+    public calculateScore(){
+
+    }
+    public showFreeCorners(){
+
+    }
+    public calculateFinalPoint(){
+
+    }
+    public verifyResorcePoint(){
+
+    }
+
+    public countAngleOccupied(){
+
+    }
+
+    public verifyObjectNeeded(){
+
+    }
+
+    public ControllPositioning(){
+
+    }
+    public showCardDispositioning(){
+
+
+    }
+
 }
