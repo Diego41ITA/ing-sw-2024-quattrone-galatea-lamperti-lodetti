c1aec4af2b9289e04f482b50bad26271de75ebd8
AlessandroLodetti <alessandro.lodetti@mail.polimi.it>
2024-03-29 13:31:39 +0100

    constructor, redefine some methods return type and i started to implement join() methods

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/gameDataManager/Matches.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/gameDataManager/Matches.java
@@ -1,21 +1,30 @@
-/**
- * This is the class that manages all the game (ongoing, suspended and ended)
- * @author Lodetti Alessandro
- */
 package it.polimi.ingsw.model.gameDataManager;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+ * This is the class that manages all the game (ongoing, suspended and ended). The user should be able to join a game
+ * only by using this class, instruction like Game.join() should be avoided: this class often returns a list of
+ * gameID that the client can use to join or to operate on that particular game.
+ * @author Lodetti Alessandro
+ */
 public class Matches {
-    private List<Game> games;
+    private final List<Game> games;
 
     /**
      * it builds an empty ArrayList
      */
     public Matches() {
         this.games = new ArrayList<>();
+    }
+
+    /**
+     * a constructor to restore the list of game in case the server crashes.
+     * @param matches it's the list of the saved ongoing/suspended game.
+     */
+    public Matches(List<Game> matches){
+        this.games = new ArrayList<>(matches);
     }
 
     /**
@@ -32,14 +41,49 @@ public class Matches {
         }
     }
 
+    private void addNewGame(){
+        Game g = new Game();
+        games.add(g);
+    }
+
+
     /**
      * a method that helps to know which games are waiting for players.
-     * @return a List of game.
+     * @return a List of String: the gameID.
      */
-    public List<Game> getGamesNotStartedYet(){
+    public List<String> getGamesNotStartedYet(){
         return games.stream().filter(game -> !game.isStarted())
+                .map(Game::getId)
                 .collect(Collectors.toList());
     }
+
+    /**use to find all the game where there is only one active player
+     * @return a list of Game.
+     */
+    public List<String> getSuspendedGames(){
+        return games.stream().filter(Game::isSuspended)
+                .map(Game::getId)
+                .collect(Collectors.toList());
+    }
+
+    public synchronized void joinRandomGame(Player player){
+        this.games.stream().filter(game -> !game.isStarted())
+                .findFirst()
+                .ifPresent(game -> game.addPlayer(player));
+
+
+    }
+    public void joinGame(String gameId){
+
+    }
+
+    public void joinSuspendedGame(String gameId, String name){
+
+    }
+
+
+
+
 
     /**
      * metodo da discutere perchè hanno detto che il nome deve essere diverso all'interno della partita e
