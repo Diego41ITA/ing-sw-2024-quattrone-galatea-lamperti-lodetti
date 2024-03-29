157c6620f104cafc8854be98414cb936042c58ff
AlessandroLodetti <alessandro.lodetti@mail.polimi.it>
2024-03-29 13:28:00 +0100

    Coord class deleted and some little changes in GameStation

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/gameDataManager/GameStation.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/gameDataManager/GameStation.java
@@ -1,12 +1,13 @@
 package it.polimi.ingsw.model.gameDataManager;
 import it.polimi.ingsw.model.card.*;
 import java.util.*;
+import java.awt.Point;
 
 /** @author 
  * this class represents the Station where the player places his cards for make point
  */
 class GameStation {
-    private Map<Card, List<Coord>> playedCards;
+    private final Map<Card, List<Point>> playedCards;
 
     public GameStation() {
         playedCards = new HashMap<>();
