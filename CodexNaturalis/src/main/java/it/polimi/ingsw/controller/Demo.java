64c6c7fb6ed46c8fb786767e64c8133c6f0b1d61
AlessandroLodetti <alessandro.lodetti@mail.polimi.it>
2024-04-05 18:10:34 +0200

    comment about the package

--- CodexNaturalis/src/main/java/it/polimi/ingsw/controller/Demo.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller/Demo.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.controller;
 
+//qui ci sta tutta la roba del controller: quindi gli stati del gioco processa le richieste del client
+// e aggiorna il model
+
 public class Demo {
 
 }
