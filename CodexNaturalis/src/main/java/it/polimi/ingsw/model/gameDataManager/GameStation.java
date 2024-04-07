1315a62aa93064abe2fe59fa35f7ed554c76b5a0
AlessandroLodetti <alessandro.lodetti@mail.polimi.it>
2024-04-07 18:21:18 +0200

    add some methods and constructor.

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/gameDataManager/GameStation.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/gameDataManager/GameStation.java
@@ -6,50 +6,82 @@ import java.awt.Point;
 /** @author 
  * this class represents the Station where the player places his cards for make point
  */
-class GameStation {
+public class GameStation {
     private final Map<Point, PlayableCard> playedCards;
-    private ArrayList<Point> FreeCords;
+    private ArrayList<Point> freeCords;
 
     public GameStation(InitialCard card) {
         playedCards = new HashMap<>();
         playedCards.put(new Point(0, 0), card);
-        FreeCords = new ArrayList<>();
-        FreeCords.add(new Point(-1,1));
-        FreeCords.add(new Point(1,1));
-        FreeCords.add(new Point(-1,-1));
-        FreeCords.add(new Point(1,-1));
+        freeCords = new ArrayList<>();
+        freeCords.add(new Point(-1,1));
+        freeCords.add(new Point(1,1));
+        freeCords.add(new Point(-1,-1));
+        freeCords.add(new Point(1,-1));
     }
 
-    public void placeCard(PlayableCard card, Point coord){
-            playedCards.put(coord, card);
-            FreeCords.remove(coord);
-            updateFreeCoords(coord);
+    public GameStation(GameStation gs){
+        this.playedCards = new HashMap<>();
+        setPlayedCards(gs.getPlayedCards());
+        this.freeCords = new ArrayList<>(gs.getFreeCords());
+    }
+
+    /**
+     * useful to know which cards have been played.
+     * @return a copy of the disposition of the card
+     */
+    public Map<Point, PlayableCard> getPlayedCards(){
+        return new HashMap<>(this.playedCards);
+    }
+
+    /**
+     * this method is useful to set the playedCards by making sure that nobody has access to the private attribute
+     * @param map is a map of played card to copy.
+     */
+    public void setPlayedCards(Map<Point, PlayableCard> map){
+        for(Point p: map.keySet()){
+            this.playedCards.put(new Point(p), map.get(p)); //get(p) is already a copy of the reference
+        }
+    }
+
+    /**
+     * returns the free coordinates
+     * @return returns a copy of the available coordinates.
+     */
+    public List<Point> getFreeCords(){
+        return new ArrayList<>(this.freeCords);
+    }
+
+    public void placeCard(PlayableCard card, Point cord){
+            playedCards.put(cord, card);
+            freeCords.remove(cord);
+            updateFreeCoords(cord);
     }
 
     //da sistemare
-    public void updateFreeCoords(Point coord){
-        Point a = new Point(coord.x - 1, coord.y - 1);
-        Point b = new Point(coord.x - 1, coord.y + 1);
-        Point c = new Point(coord.x + 1, coord.y - 1);
-        Point d = new Point(coord.x + 1, coord.y + 1);
+    public void updateFreeCoords(Point cord){
+        Point a = new Point(cord.x - 1, cord.y - 1);
+        Point b = new Point(cord.x - 1, cord.y + 1);
+        Point c = new Point(cord.x + 1, cord.y - 1);
+        Point d = new Point(cord.x + 1, cord.y + 1);
 
         if(!playedCards.containsKey(a)){
-            FreeCords.add(a);
+            freeCords.add(a);
         }else {
             playedCards.get(a).hideAngle(Angle.HIGHRIGHT);
         }
         if(!playedCards.containsKey(b)) {
-            FreeCords.add(b);
+            freeCords.add(b);
         }else {
             playedCards.get(b).hideAngle(Angle.DOWNRIGHT);
         }
         if(!playedCards.containsKey(c)){
-            FreeCords.add(c);
+            freeCords.add(c);
         }else {
             playedCards.get(c).hideAngle(Angle.HIGHLEFT);
         }
         if(!playedCards.containsKey(d)){
-            FreeCords.add(d);
+            freeCords.add(d);
         }else {
             playedCards.get(d).hideAngle(Angle.DOWNLEFT);
         }
