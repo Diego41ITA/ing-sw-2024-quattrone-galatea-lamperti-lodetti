2ca111f59f63ba777f2ebe74cedd2cb4245bb3d9
Luca <l.lamperti2002@gmail.com>
2024-04-10 15:12:16 +0200

    PositionCheck update

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/card/strategyPattern/PositionCheck1.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/card/strategyPattern/PositionCheck1.java
@@ -8,12 +8,14 @@ import java.awt.*;
 import java.util.HashMap;
 
 public class PositionCheck1 implements CheckInterface {
+
+    boolean flag;
+    int i;
     @Override
     public boolean check(HashMap<Point, PlayableCard> PlayedCard, HashMap<Item, Integer> AvailableItems, HashMap<Item, Integer> requirements) {
         for (Point a : PlayedCard.keySet()){
             if (PlayedCard.get(a).getType().equals(TypeOfCard.MUSHROOM)){
-                boolean flag = true;
-                int i;
+                flag = true;
                 for (i=1; i<3; i++){
                     Point nextPoint = new Point(a.x+i,a.y+i);
                     if (!PlayedCard.containsKey(nextPoint) || !PlayedCard.get(nextPoint).getType().equals(TypeOfCard.MUSHROOM)){
