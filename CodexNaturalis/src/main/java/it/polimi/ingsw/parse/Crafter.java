56b35a825dce207104791e08f114e111f349936f
AleLodetti <lodetti.alessandro02@gmail.com>
2024-04-12 15:06:05 +0200

    json

--- CodexNaturalis/src/main/java/it/polimi/ingsw/parse/Crafter.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/parse/Crafter.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.parse;
 import com.google.gson.Gson;
-import it.polimi.ingsw.model.card.GoldCard;
+import it.polimi.ingsw.model.card.*;
+import it.polimi.ingsw.model.card.strategyPattern.*;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
@@ -24,8 +25,9 @@ public class Crafter {
         //ora il file è creato bisogna creare tutto quello che serve a riempirlo
 
         //carte oro
-        HashMap> g;
-        List >
+        HashMap<>g;
+        List<> l;
+
         GoldCard gc = new GoldCard();
 
         //carte risorsa
@@ -33,6 +35,24 @@ public class Crafter {
         //carte start
 
         //carte obiettivo
+        Object[][] matrix = {
+                {TypeOfCard.MUSHROOM, false, 2, positionCheck1, null},
+                {TypeOfCard.VEGETABLE, false, 2, positionCheck2, null},
+                {TypeOfCard.MUSHROOM, false, 2, positionCheck3, null},
+                {TypeOfCard.MUSHROOM, false, 2, positionCheck4, null},
+                {TypeOfCard.MUSHROOM, false, 2, positionCheck5, null},
+                {TypeOfCard.MUSHROOM, false, 2, positionCheck6, null},
+                {TypeOfCard.MUSHROOM, false, 2, positionCheck7, null},
+                {TypeOfCard.MUSHROOM, false, 2, positionCheck8, null},
+                {TypeOfCard.MUSHROOM, false, 2,null, null},
+                {TypeOfCard.MUSHROOM, false, 2, positionCheck, null},
+                {TypeOfCard.MUSHROOM, false, 2, positionCheck1, null},
+                {TypeOfCard.MUSHROOM, false, 2, positionCheck1, null},
+                {TypeOfCard.MUSHROOM, false, 2, positionCheck1, null},
+                {TypeOfCard.MUSHROOM, false, 2, positionCheck1, null},
+                {TypeOfCard.MUSHROOM, false, 2, positionCheck1, null},
+                {TypeOfCard.MUSHROOM, false, 2, positionCheck1, null},
+        };
 
         //ora c'è il file e ci sono gli oggetti da metterci dentro, si utilizzando i comandi di Gson
 
