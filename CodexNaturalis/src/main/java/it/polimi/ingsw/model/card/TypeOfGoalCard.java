ed61f5409f7c779cf7e6a833748ff3851488dc11
Luca <l.lamperti2002@gmail.com>
2024-04-14 12:25:49 +0200

    delete enum for GoalCard and positioning

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/card/TypeOfGoalCard.java
+++
@@ -1,9 +0,0 @@
-package it.polimi.ingsw.model.card;
-
-/**
- * @author Luca Lamperti
- * an enumeration to implement which type of goal is required by the GoalCard
- */
-public enum TypeOfGoalCard {
-    OBJECT, POSITION, RESOURCE
-}
