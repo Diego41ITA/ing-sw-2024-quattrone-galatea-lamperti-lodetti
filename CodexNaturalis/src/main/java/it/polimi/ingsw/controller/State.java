3cf61e1eca8c093297e0fc0fcb64e44354927fe2
LorenzoGalatea <161478338+LorenzoGalatea@users.noreply.github.com>
2024-04-27 10:23:04 +0200

    Update State.java

--- CodexNaturalis/src/main/java/it/polimi/ingsw/controller/State.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/controller/State.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.controller;
 
 abstract public class State {
+    Static State SetupGame, PlayerTurn, CheckPoint, CalculateWinner,  UpdateGame;
 
     public abstract void HandleInput(String input);
     //now we should define all the methods.
