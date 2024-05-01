e6a8b04545d5dac994caa4059a65599f6460fdb5
AlessandroLodetti <alessandro.lodetti@mail.polimi.it>
2024-05-01 17:50:54 +0200

    aggiunta la classe e commentato GameFlow

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/GameFlow.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GameFlow.java
@@ -4,4 +4,16 @@ import it.polimi.ingsw.networking2.ClientAction;
 import it.polimi.ingsw.observer.GameObserver;
 
 public class GameFlow implements Runnable, ClientAction, GameObserver {
+    private ClientAction client;
+    private String nickname;
+    private final Ui ui;
+
+    //tutto il flusso di gioco....
+
+    //implementa i metodi di gameObserver per svolgere finalmente delle operazioni concrete in risposta alle notifiche
+    //che arrivano dal model
+
+    //implementa runnable perché il cuore starà dentro ad un thread
+
+    //implementa clientAction perchè chiamerà quei metodi sull'oggetto client (Socket o RMI)
 }
---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GameObserverHandlerClient.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.view;
+
+import it.polimi.ingsw.model.GameView;
+import it.polimi.ingsw.observer.GameObserver;
+
+import java.io.Serializable;
+import java.rmi.RemoteException;
+
+//implementa GameObserver e nel caso di RMI viene esportata in modo che quando si effettua una chiamata a metodi di
+//GameObserver si comporta come un oggetto remoto => ha come effetto quello di chiamare metodi di flow di conseguenza
+//notifica il flusso di gioco.
+public class GameObserverHandlerClient implements GameObserver, Serializable {
+    private GameFlow flow;
+    public GameObserverHandlerClient(GameFlow flow){
+        this.flow = flow;
+    }
+
+    @Override
+    public void playerJoined(GameView model) throws RemoteException{
+        flow.playerJoined(model);
+    }
+
+    //Override di tutti gli altri messaggi che il server può mandare al client
+}
