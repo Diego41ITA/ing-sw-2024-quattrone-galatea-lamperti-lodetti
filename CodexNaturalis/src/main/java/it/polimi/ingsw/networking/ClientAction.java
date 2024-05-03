a6532633de7fcfb88291f205f12c650973ebfa47
AlessandroLodetti <alessandro.lodetti@mail.polimi.it>
2024-05-03 19:08:24 +0200

    i added the methods we need to discuss how to implement some methods (I suggest a light serialization, use int instead of Object).

--- CodexNaturalis/src/main/java/it/polimi/ingsw/networking/ClientAction.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/networking/ClientAction.java
@@ -3,17 +3,17 @@ package it.polimi.ingsw.networking;
 import it.polimi.ingsw.model.card.PlayableCard;
 
 import java.awt.*;
+import java.rmi.RemoteException;
 import java.util.List;
 
 //questa interfaccia indica tutte le azioni possibili effettuabili da un client
 //notate che rappresenta il punto di contatto tra ClientRMI e ClientSocket perchè entrambi la estenderanno
 public interface ClientAction {
-    void createGame(String nick);
-    void joinRandomGame(String nick);
-    void joinGame(String nick, String gameId);
-    void rejoin(String nick, String gameId);
+    void createGame(String nick) throws RemoteException;
+    void joinRandomGame(String nick) throws RemoteException;
+    void rejoin(String nick, String gameId) throws RemoteException;
     void setMaxNumOfPlayer(String name, int max);
-    void playCard(PlayableCard card, Point cord, String nick);
+    void playCard(int card, Point cord, String nick);
     void chooseGoal(List<PlayableCard> goals, int num, String nick); //pensare ad una notify che restituisca due goal card
     //void chooseGoal(int num, String nick);
 
---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/networking/rmi/ClientRMI.java
@@ -0,0 +1,107 @@
+package it.polimi.ingsw.networking.rmi;
+import it.polimi.ingsw.controller.*;
+import it.polimi.ingsw.model.card.PlayableCard;
+import it.polimi.ingsw.networking.ClientAction;
+import it.polimi.ingsw.observer.GameObserver;
+import it.polimi.ingsw.view.GameFlow;
+import it.polimi.ingsw.view.GameObserverHandlerClient;
+
+import java.awt.*;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+//si occupa di implementare le azioni del client lavorando sulla rete.
+public class ClientRMI implements ClientAction {
+    //tutti i client istanziati su questa macchina avranno lo stesso riferimento
+    private static MainControllerInterface request;
+    private GameControllerInterface gameController = null;
+
+    //notificationGetter è un oggetto su cui il SERVER chiamerà i metodi remoti (per notificare che è successo qualcosa)
+    private GameObserver notificationGetter;
+    private GameObserverHandlerClient gameObserverHandler;
+
+    private String nickname;
+    private Registry registry;
+    private GameFlow flow;
+
+    //ora ho il costruttore piu dei metodi ausiliari.
+    public ClientRMI(GameFlow flow){
+        super();
+        gameObserverHandler = new GameObserverHandlerClient(flow);
+        connect(); //connette il client con il server RMI
+
+        this.flow = flow;
+    }
+
+    public void connect(){
+        //va reso più solido chiaramente
+        try{
+            registry = LocateRegistry.getRegistry(/*...*/);
+            request = (MainControllerInterface) registry.lookup("nome del server");
+
+            notificationGetter = (GameObserver) UnicastRemoteObject.exportObject(gameObserverHandler, 0);
+
+            //il client è pronto
+        }catch(Exception e){
+            //fa qualcosa...
+        }
+    }
+
+    //bisogna implementare i metodi di ClientAction
+    @Override
+    public void createGame(String nick) throws RemoteException{
+        registry = LocateRegistry.getRegistry(/*...*/);
+        request = (MainControllerInterface) registry.lookup("server name");
+        gameController = request.createGame(notificationGetter, nick);
+        nickname = nick;
+    }
+
+    @Override
+    public void joinRandomGame(String nick) throws  RemoteException{
+        registry = LocateRegistry.getRegistry(/*...*/);
+        request = (MainControllerInterface) registry.lookup("server name");
+        gameController = request.joinRandomGame(notificationGetter, nick);
+        nickname = nick;
+    }
+
+    @Override
+    public void rejoin(String nick, String gameId) throws RemoteException{
+        registry = LocateRegistry.getRegistry(/*...*/);
+        request = (MainControllerInterface) registry.lookup("server name");
+        gameController = request.rejoin(notificationGetter, nick, gameId);
+        nickname = nick;
+    }
+
+    //metodi del GameController
+    @Override
+    public void setMaxNumOfPlayer(String name, int max){
+        gameController.setMaxNumberPlayers(name, max);
+    }
+
+    @Override
+    public void playCard(int numberOfCard, Point cord, String nick){
+        gameController.playCard(numberOfCard, cord, nick); //per visualizzazione (anche TUI) sarebbe meglio
+        //lavorare con degli int piuttosto che con tutto l'oggetto card (tanto il controller sa quali carte
+        // ha in mano il giocatore).
+    }
+
+    @Override
+    public void chooseGoal()...
+
+    @Override
+    public void pass(String nick){
+        //...
+    }
+
+    @Override
+    public void drawFromDeck(String nick, String deck){
+        ...
+    }
+
+    @Override
+    public void drawFromTable(String nick, int card){
+        ...
+    }
+}
