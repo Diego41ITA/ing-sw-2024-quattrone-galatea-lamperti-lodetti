6de63d410ba442f52f6c20c927fb80006b2f6d0b
AlessandroLodetti <alessandro.lodetti@mail.polimi.it>
2024-05-04 10:41:22 +0200

    this class handles the notifications from the server to the client

--- CodexNaturalis/src/main/java/it/polimi/ingsw/networking/socket/server/GameObserverHandlerSocket.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/networking/socket/server/GameObserverHandlerSocket.java
@@ -1,4 +1,29 @@
 package it.polimi.ingsw.networking.socket.server;
 
-public class GameObserverHandlerSocket {
+import it.polimi.ingsw.model.GameView;
+import it.polimi.ingsw.observer.GameObserver;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.rmi.RemoteException;
+
+public class GameObserverHandlerSocket implements GameObserver, Serializable {
+    private final ObjectOutputStream out;
+    public GameObserverHandlerSocket(ObjectOutputStream o){
+        this.out = o;
+    }
+
+    //Bisogna fare l'override dei metodi di gameObserver, come per GameObserverHandlerClient bisgnerà inviare un
+    //messaggio questo verrà scritto sull'OutputStream
+    @Override
+    public void playerJoined(GameView game) throws RemoteException{
+        try{
+            out.writeObject(new msgPlayerJoined(game));
+        }catch(IOException e){
+            //
+        }
+    }
+
+    //ecc...
 }
