bc1a1619029f451b2f6be422378f68c4819f1cd4
AlessandroLodetti <alessandro.lodetti@mail.polimi.it>
2024-05-04 12:03:17 +0200

    just a comment

--- CodexNaturalis/src/main/java/it/polimi/ingsw/networking/socket/server/serverToClientMessage/ServerNotification.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/networking/socket/server/serverToClientMessage/ServerNotification.java
@@ -5,7 +5,7 @@ import it.polimi.ingsw.observer.GameObserver;
 import java.io.IOException;
 import java.io.InterruptedIOException;
 import java.io.Serializable;
-
+//this is a generic server notification
 public abstract class ServerNotification implements Serializable {
     public abstract void execute(GameObserver obs) throws IOException, InterruptedException;
 }
