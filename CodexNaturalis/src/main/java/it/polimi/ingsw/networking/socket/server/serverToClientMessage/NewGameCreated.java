9c9ded227699c397f87641d6df9b794b981d9cf1
AlessandroLodetti <alessandro.lodetti@mail.polimi.it>
2024-05-08 18:06:05 +0200

    Messages server to client

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/networking/socket/server/serverToClientMessage/NewGameCreated.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw.networking.socket.server.serverToClientMessage;
+
+import it.polimi.ingsw.observer.GameObserver;
+
+import java.io.IOException;
+
+public class NewGameCreated extends ServerNotification{
+    private final String gameId;
+    public NewGameCreated(String gameId){
+        this.gameId = gameId;
+    }
+    @Override
+    public void execute(GameObserver obs) throws IOException, InterruptedException{
+        obs.newGameCreated(gameId);
+    }
+}
---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/networking/socket/server/serverToClientMessage/UpdateColor.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.networking.socket.server.serverToClientMessage;
+
+import it.polimi.ingsw.model.GameView;
+import it.polimi.ingsw.observer.GameObserver;
+
+import java.io.IOException;
+
+public class UpdateColor extends ServerNotification{
+    private final GameView game;
+    public UpdateColor(GameView g){
+        this.game = g;
+    }
+
+    @Override
+    public void execute(GameObserver obs) throws IOException, InterruptedException{
+        obs.updateColor(game);
+    }
+}
