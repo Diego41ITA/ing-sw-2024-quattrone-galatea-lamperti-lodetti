8a668f21f60483cbf4d1ca27831b5642e6702c75
AlessandroLodetti <alessandro.lodetti@mail.polimi.it>
2024-05-09 19:19:48 +0200

    graphic content

--- CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/controller/MenuController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/controller/MenuController.java
@@ -5,6 +5,8 @@ import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+//forse sta cosa si può togliere: non è necessario implementare Initializable, però la classe MenuController per gestire
+//la logica è necessaria.
 public class MenuController implements Initializable{
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
