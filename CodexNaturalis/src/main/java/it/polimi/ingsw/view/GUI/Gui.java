3eb83d168da906a4ce29907869da083573170733
AlessandroLodetti <alessandro.lodetti@mail.polimi.it>
2024-05-09 17:08:32 +0200

    two class for the gui

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/Gui.java
@@ -0,0 +1,157 @@
+package it.polimi.ingsw.view.GUI;
+
+import it.polimi.ingsw.model.GameView;
+import it.polimi.ingsw.model.card.GoalCard;
+import it.polimi.ingsw.model.card.PlayableCard;
+import it.polimi.ingsw.model.gameDataManager.GameStation;
+import it.polimi.ingsw.view.UI;
+
+/* questa classe in base al metodo dovrà runnare la grafica su thread e bisognerà
+ */
+public class Gui extends UI {
+
+    @Override
+    public void show_startingMenu() {
+
+    }
+
+    @Override
+    public void show_message(String message) {
+
+    }
+
+    @Override
+    public void show_joinRandomGame() {
+
+    }
+
+    @Override
+    public void show_noAvailableGames() {
+
+    }
+
+    @Override
+    public void show_createNewGame() {
+
+    }
+
+    @Override
+    public void show_reconnectGame() {
+
+    }
+
+    @Override
+    public void show_invalidIdGame() {
+
+    }
+
+    @Override
+    public void show_NickAlreadyUsed(GameView gameView) {
+
+    }
+
+    @Override
+    public void show_invalidNickToReconnect(String id) {
+
+    }
+
+    @Override
+    public void show_currentPlayersStatus(GameView gameView) {
+
+    }
+
+    @Override
+    public void show_playerColors(GameView gameView) {
+
+    }
+
+    @Override
+    public void show_GameStatus(GameView gameView) {
+
+    }
+
+    @Override
+    public void show_RequestPlayerNickName() {
+
+    }
+
+    @Override
+    public void show_RequestGameId() {
+
+    }
+
+    @Override
+    public void show_RequestNumberOfPlayers() {
+
+    }
+
+    @Override
+    public void show_playerJoined(String id) {
+
+    }
+
+    @Override
+    public void show_playerLeft(String playerNickName) {
+
+    }
+
+    @Override
+    public void show_gameStarting(String id) {
+
+    }
+
+    @Override
+    public void show_isYourTurn(GameView immutableModel) {
+
+    }
+
+    @Override
+    public void show_playerHand(GameView immutableModel) {
+
+    }
+
+    @Override
+    public void show_gameStation(GameStation gameStation) {
+
+    }
+
+    @Override
+    public void show_invalidPlay() {
+
+    }
+
+    @Override
+    public void show_notEnoughResources() {
+
+    }
+
+    @Override
+    public void show_goalCard(GoalCard card) {
+
+    }
+
+    @Override
+    public void show_playableCard(PlayableCard card) {
+
+    }
+
+    @Override
+    public void show_tableOfDecks(GameView immutableModel) {
+
+    }
+
+    @Override
+    public void show_lastTurn() {
+
+    }
+
+    @Override
+    public void show_pointTable(GameView immutableModel) {
+
+    }
+
+    @Override
+    public void show_gameOver() {
+
+    }
+}
---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/GuiApplication.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.view.GUI;
+import javafx.application.Application;
+
+public class GuiApplication extends Application{
+}
