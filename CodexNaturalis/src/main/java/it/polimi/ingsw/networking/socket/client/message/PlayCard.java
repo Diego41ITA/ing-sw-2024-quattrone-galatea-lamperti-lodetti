57a53d2b0ad599b087177d23a135b91682edc2ff
AlessandroLodetti <alessandro.lodetti@mail.polimi.it>
2024-05-11 10:40:22 +0200

    fix

--- CodexNaturalis/src/main/java/it/polimi/ingsw/networking/socket/client/message/PlayCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/networking/socket/client/message/PlayCard.java
@@ -4,6 +4,7 @@ import it.polimi.ingsw.controller.GameControllerInterface;
 import it.polimi.ingsw.controller.MainControllerInterface;
 import it.polimi.ingsw.model.card.PlayableCard;
 import it.polimi.ingsw.model.exceptions.GameEndedException;
+import it.polimi.ingsw.model.exceptions.illegalOperationException;
 import it.polimi.ingsw.observer.GameObserver;
 
 import java.awt.*;
@@ -24,7 +25,11 @@ public class PlayCard extends Message{
     @Override
     public void execute(GameControllerInterface game) throws RemoteException, GameEndedException {
         //if first card, I maybe need to set the GameStation
-        game.playCard(playedCard, nickname, isFront, point); //illegal operation exception needs to be transformed in
+        try {
+            game.playCard(playedCard, nickname, isFront, point); //illegal operation exception needs to be transformed in
+        } catch (illegalOperationException e) {
+            throw new RuntimeException(e);
+        }
         //RemoteException
     }
 
