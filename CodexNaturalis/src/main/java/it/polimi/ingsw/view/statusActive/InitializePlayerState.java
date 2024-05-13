b78ab879992e91cd6a09d0d5a3e2ac7d80d2fdea
Diego41ITA <diego.quattrone@mail.polimi.it>
2024-05-13 20:22:18 +0200

    added InitializePlayerState

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/statusActive/InitializePlayerState.java
@@ -0,0 +1,25 @@
+package it.polimi.ingsw.view.statusActive;
+
+import it.polimi.ingsw.model.GameView;
+import it.polimi.ingsw.view.GameFlow;
+
+public class InitializePlayerState extends StateActive{
+    protected InitializePlayerState(GameFlow flow) {
+        super(flow);
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public void nextState() {
+
+    }
+
+    @Override
+    public void setView(GameView view) {
+
+    }
+}
