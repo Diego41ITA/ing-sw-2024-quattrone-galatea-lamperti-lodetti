a8e0d3df5f0b8f3bbc523bcdcd4485674b93ace8
Lorenzo galatea <lorenzo.galatea@mail.polimi.it>
2024-05-17 15:06:57 +0200

    other scenes and other controllers

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/controller/InitialCardController.java
@@ -0,0 +1,41 @@
+package it.polimi.ingsw.view.GUI.controller;
+import it.polimi.ingsw.model.gameDataManager.Color;
+import it.polimi.ingsw.view.GUI.Gui;
+import javafx.fxml.FXML;
+import javafx.scene.control.TabPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.event.*;
+import javafx.scene.text.Text;
+
+import static it.polimi.ingsw.view.GUI.ImageAssociator.associatorPng2Card;
+import static it.polimi.ingsw.view.GUI.ImageAssociator.makerAssociator;
+public class InitialCardController extends Gui{
+
+    @FXML
+    ImageView imageFront;
+
+    @FXML
+    ImageView imageBack;
+
+    public void setUp(int id){
+        imageBack.setImage(new Image(associatorPng2Card(String.valueOf(id),false)));
+        imageFront.setImage(new Image(associatorPng2Card(String.valueOf(id),true)));
+    }
+    // se con il mouse clicco sulla immagine allora mette true in multipleResponses
+    public void selectFront(MouseEvent e){
+        multipleResponses.add("true");
+
+    }
+    // se con il mouse clicco sulla immagine allora mette false in multipleResponses
+    public void selectBack(MouseEvent e){
+        multipleResponses.add("false");
+
+    }
+
+
+}
---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/view/GUI/controller/PlayerGoalController.java
@@ -0,0 +1,6 @@
+package it.polimi.ingsw.view.GUI.controller;
+
+public class PlayerGoalController {
+
+
+}
